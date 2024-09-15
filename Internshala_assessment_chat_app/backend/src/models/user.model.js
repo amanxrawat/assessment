a7e597180de0fc42e6a0fe77c111a9263ef7e0da
amanxrawat <amanxrawat.93@gmail.com>
2024-09-15 17:02:12 +0530

    ``` Initial setup for backend: - Added user model with schema and methods - Implemented JWT authentication middleware - Configured environment variables and database connection - Set up Express app with middleware and CORS - Created utility classes for API error handling and async operations - Added package dependencies and scripts - Included .gitignore and .env.sample files

---
+++ Internshala_assessment_chat_app/backend/src/models/user.model.js
@@ -0,0 +1,93 @@
+import mongoose from 'mongoose';
+
+// creating the userSchema
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 3,
+        maxlength: 30
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    friendList: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+    friendRequestsSent: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+    friendRequestsReceived: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+    interests: {
+        type: [String],
+        lowercase: true,
+        default: []
+    },
+    profilePicture: {
+        type: String,
+        default: ''
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+}, { timestamps: true });
+
+
+userSchema.virtual('mutualFriends').get(function (otherUser) {
+    const mutualFriends = this.friendList.filter(friend =>
+        otherUser.friendList.includes(friend.toString())
+    );
+    return mutualFriends.length;
+});
+
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password")){
+        return next();
+    }
+    this.password = await bcrypt.hash(this.password,10)
+    next();
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password,this.password);
+
+}
+
+userSchema.methods.generateAccessToken = async function(){
+    return await jsonwebtoken.sign(
+        {
+            _id:this._id,
+            email:this.email,
+            fullName:this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:ACCESS_TOKEN_EXPIRY
+        }
+    )
+} 
+
+
+
+const User = mongoose.model('User', userSchema);
+export { User };
